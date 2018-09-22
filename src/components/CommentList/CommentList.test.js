2e6c5315446dd50c768b1bcf9b60c07ddfe7fb93
Andy Edwards <andyhedwards@gmail.com>
2018-09-22 13:54:32 +0100

    Create CommentList and CommentListItem components

---
+++ src/components/CommentList/CommentList.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentList from './CommentList';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<CommentList />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Dashboard.css';
 
+import CommentList from '../CommentList/CommentList';
+
 const Dashboard = () => {
   return (
     <div id='dash-board'>
-      
+      <CommentList />
     </div>
   );
 }
